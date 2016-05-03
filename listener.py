582f8721111b1e5ca486d8dda079b22af47f9f90
mattghent <matt.ghent@invitae.com>
2016-05-02 21:27:54 -0700

    refactor

--- listener.py
+++ listener.py
@@ -17,16 +17,47 @@ class StdOutListener(StreamListener):
 
     def on_data(self, data):
         data = json.loads(data)
-        if 'user' in data and 'text' in data:
-            tweet = Tweet(data['text'], data['user']['name'], data['user']['screen_name'],
-                          data['user']['id_str'], data['user']['verified'], data['timestamp_ms'],
-                          data['entities']['hashtags'], data['user']['friends_count'], data['user']['followers_count'])
-        db.session.add(tweet)
-        db.session.commit()
+        tweet_id = self.write_tweet(**data)
+        if tweet_id is not None:
+            # Add hashtag
+            self.write_hashtags(tweet_id, data['entities']['hashtags'])
+
         return True
 
     def on_error(self, status):
         print(status)
+
+    @staticmethod
+    def write_tweet(data):
+        """
+        Takes dictionary and writes it to the database
+        :param tweet: dictionary containing the following fields
+            'text' - the content of the tweet
+            'user'['name'] = twitter real name
+            'user'['screen_name'] = twitter screen name
+            'user'['id_str'] = twitter id string
+            'user'['verified] = Is this user verified (blue checkmark)
+            'timestamp' = time stamp (unix time)
+            'user'['friends_count'] = How many people this user follows
+            'user'['followers_count'] = How many people follow this user
+        :return: tweet id
+        """
+        if 'user' in data and 'text' in data:
+            tweet = Tweet(tweet=data['text'], name=data['user']['name'], username=data['user']['screen_name'],
+                          user_id=data['user']['id_str'], verified=data['user']['verified'],
+                          timestamp=data['timestamp_ms'], following=data['user']['friends_count'],
+                          followers=data['user']['followers_count'])
+            db.session.add(tweet)
+            db.session.commit()
+            return tweet.id
+        return None
+
+    @staticmethod
+    def write_hashtags(tweet_id, hashtag):
+        """
+        :return:
+        """
+        pass
 
 
 def run(search_list):
